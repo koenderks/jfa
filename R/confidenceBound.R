f11d2792b71b6927b9d60afec59c260a0301043b
Koen Derks <koen-derks@hotmail.com>
2019-11-30 13:37:13 +0100

    add template for confidenceBound function

---
+++ R/confidenceBound.R
@@ -0,0 +1,24 @@
+#' Audit confidence / credible bounds
+#'
+#' This function 
+#'
+#' @usage confidenceBound()
+#'
+#' @param 
+#'
+#' @return A list containing the confidence bound for the audit.
+#'
+#' @author Koen Derks, \email{k.derks@nyenrode.nl}
+#'
+#' @seealso
+#'
+#' @references
+#'
+#' @examples
+#' 
+#' 
+
+confidenceBound <- function(){
+  
+  return()
+}
