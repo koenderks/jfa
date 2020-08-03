19053bd3ae2dae9b658cc1ada5bb5fd7bf8a8694
Koen Derks <koen-derks@hotmail.com>
2020-08-03 19:47:27 +0200

    Update last unit test for release

--- tests/testthat/test-planning.R
+++ tests/testthat/test-planning.R
@@ -142,3 +142,71 @@ test_that(desc = "Bayesian hypergeometric 5% materiality 2.5% errors custom prio
   expect_equal(jfaRes$sampleSize, 193)
   expect_equal(jfaRes$expectedSampleError, 5)
 })
+
+test_that(desc = "Bayesian binomial 2% precision 5% materiality 2.5% errors median prior", {
+  jfaPrior <- auditPrior(materiality = 0.05, confidence = 0.95, method = "median", expectedError = 0.025)
+  jfaRes <- planning(materiality = 0.05, minPrecision = 0.02, confidence = 0.95, expectedError = 0.025, likelihood = "binomial", N = 1000, prior = jfaPrior)
+  print(jfaRes)
+  plot(jfaRes)
+  expect_equal(jfaRes$sampleSize, 283)
+  expect_equal(jfaRes$expectedSampleError, 7.08)
+})
+
+test_that(desc = "Bayesian binomial 5% materiality 1% errors hypotheses prior", {
+  jfaPrior <- auditPrior(materiality = 0.05, confidence = 0.95, method = "hypotheses", pHmin = 0.7)
+  jfaRes <- planning(materiality = 0.05, confidence = 0.95, expectedError = 0.01, likelihood = "binomial", N = 1000, prior = jfaPrior)
+  print(jfaRes)
+  plot(jfaRes)
+  expect_equal(jfaRes$sampleSize, 55)
+  expect_equal(jfaRes$expectedSampleError, 0.55)
+})
+
+test_that(desc = "Bayesian poisson 5% materiality 1% errors factor prior", {
+  jfaPrior <- auditPrior(materiality = 0.05, confidence = 0.95, method = "factor", factor = 0.6, sampleN = 58, sampleK = 0, expectedError = 0.025)
+  jfaRes <- planning(materiality = 0.05, confidence = 0.95, expectedError = 0.01, likelihood = "binomial", N = 1000, prior = jfaPrior)
+  print(jfaRes)
+  plot(jfaRes)
+  expect_equal(jfaRes$sampleSize, 36)
+  expect_equal(jfaRes$expectedSampleError, 0.36)
+})
+
+test_that(desc = "Bayesian poisson 3% materiality 1.3% errors sample prior", {
+  jfaPrior <- auditPrior(materiality = 0.03, confidence = 0.95, method = "sample", sampleN = 58, sampleK = 0, expectedError = 0.025)
+  jfaRes <- planning(materiality = 0.03, confidence = 0.95, expectedError = 0.013, likelihood = "binomial", N = 1000, prior = jfaPrior)
+  print(jfaRes)
+  plot(jfaRes)
+  expect_equal(jfaRes$sampleSize, 140)
+  expect_equal(jfaRes$expectedSampleError, 1.82)
+})
+
+test_that(desc = "Frequentist poisson 3% materiality 1.3% errors 5% min precision", {
+  jfaRes <- planning(materiality = 0.03, confidence = 0.95, expectedError = 0.013, minPrecision = 0.05, likelihood = "binomial", N = 1000)
+  print(jfaRes)
+  plot(jfaRes)
+  expect_equal(jfaRes$sampleSize, 303)
+  expect_equal(jfaRes$expectedSampleError, 4)
+})
+
+test_that(desc = "Bayesian poisson 3% materiality 1.3% errors 5% min precision standard prior", {
+  jfaRes <- planning(materiality = 0.03, confidence = 0.95, expectedError = 0.013, minPrecision = 0.05, likelihood = "binomial", N = 1000, prior = T)
+  print(jfaRes)
+  plot(jfaRes)
+  expect_equal(jfaRes$sampleSize, 293)
+  expect_equal(jfaRes$expectedSampleError, 3.81)
+})
+
+test_that(desc = "Bayesian poisson 100% materiality 0% errors 2% min precision standard prior", {
+  jfaRes <- planning(confidence = 0.95, minPrecision = 0.02, likelihood = "poisson", prior = T)
+  print(jfaRes)
+  plot(jfaRes)
+  expect_equal(jfaRes$sampleSize, 150)
+  expect_equal(jfaRes$expectedSampleError, 0)
+})
+
+test_that(desc = "Bayesian poisson 100% materiality 1% errors 2% min precision standard prior", {
+  jfaRes <- planning(confidence = 0.95, minPrecision = 0.02, expectedError = 0.01, likelihood = "poisson", prior = T)
+  print(jfaRes)
+  plot(jfaRes)
+  expect_equal(jfaRes$sampleSize, 220)
+  expect_equal(jfaRes$expectedSampleError, 2.2)
+})
