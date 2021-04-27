ef6b37051bde82a3a571357635fce02fc6761776
Koen Derks <koen-derks@hotmail.com>
2021-04-27 13:11:30 +0200

    Update unit tests validation

--- tests/testthat/test-validation-smash21.R
+++ tests/testthat/test-validation-smash21.R
@@ -14,10 +14,27 @@ test_that(desc = "(id: f10-v0.5.3-t1) Test frequentist sample sizes", {
 # Retrieved on 27-04-2021 from https://steekproeven.eu/wp-content/uploads/2021/01/SMASH21-Bayes-kopie.xlsx
 
 test_that(desc = "(id: f10-v0.5.3-t2) Test Bayesian sample sizes", {
-  ub <- 5000 / 20000 # exp.ub / N
-  theta <- 2000 / 20000 # materiality / N
-  expectedError <- 300 / 20000 # exp.error / N
-  prior <- auditPrior(confidence = 0.95, materiality = theta, likelihood = "poisson", method = "bram", expectedError = expectedError, ub = ub)
-  plan <- planning(confidence = 0.95, materiality = theta, expectedError = expectedError, prior = prior)
-  expect_equal(plan[["sampleSize"]], 28)
+  
+  N <- 20000
+  materiality <- 2000 / N
+  expectedError <- c(300, 500, 700, 900) / N
+  ub <- c(5000, 10000, 15000, 18000) / N
+  sampleSizeMatrix <- matrix(NA, nrow = length(expectedError), ncol = length(ub))
+  for (i in 1:nrow(sampleSizeMatrix)) {
+    for (j in 1:ncol(sampleSizeMatrix)) {
+      prior <- auditPrior(confidence = 0.95, materiality = materiality, likelihood = "poisson", method = "bram", expectedError = expectedError[i], ub = ub[j])
+      plan <- planning(confidence = 0.95, materiality = materiality, expectedError = expectedError[i], prior = prior)
+      sampleSizeMatrix[i, j] <- plan[["sampleSize"]]
+    }
+  }
+  
+  smash_matrix <- matrix(c(28, 35, 37, 38,
+                           38, 46, 49, 49,
+                           54, 63, 65, 66,
+                           79, 89, 92, 93),
+                         byrow = TRUE,
+                         nrow = length(expectedError),
+                         ncol = length(ub))
+  expect_equal(sampleSizeMatrix, smash_matrix)
+  
 })
