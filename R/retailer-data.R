9d455a32203d6032ec43e1818cd7daeaf718ee2b
Koen Derks <koen-derks@hotmail.com>
2022-11-16 07:16:56 +0100

    Update retailer

--- R/retailer-data.R
+++ R/retailer-data.R
@@ -13,9 +13,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-#' Retailer data
+#' Retailer Group Audit
 #'
-#' Sample outcomes per stratum from a retail company consisting of 20 branches.
+#' Sample outcomes summarized per branch from an audit of a retail company consisting of 20 branches.
 #'
 #' @docType data
 #'
