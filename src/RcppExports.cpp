d91cdd2f3d952e9ed5c694d55f961630a0d88e0b
Koen Derks <koen-derks@hotmail.com>
2022-12-13 09:22:07 +0100

    Remove poisson model

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -15,14 +15,12 @@ Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 RcppExport SEXP _rcpp_module_boot_stan_fit4cp_error_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4cp_taint_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4pp_error_mod();
-RcppExport SEXP _rcpp_module_boot_stan_fit4pp_poisson_mod();
 RcppExport SEXP _rcpp_module_boot_stan_fit4pp_taint_mod();
 
 static const R_CallMethodDef CallEntries[] = {
     {"_rcpp_module_boot_stan_fit4cp_error_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4cp_error_mod, 0},
     {"_rcpp_module_boot_stan_fit4cp_taint_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4cp_taint_mod, 0},
     {"_rcpp_module_boot_stan_fit4pp_error_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4pp_error_mod, 0},
-    {"_rcpp_module_boot_stan_fit4pp_poisson_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4pp_poisson_mod, 0},
     {"_rcpp_module_boot_stan_fit4pp_taint_mod", (DL_FUNC) &_rcpp_module_boot_stan_fit4pp_taint_mod, 0},
     {NULL, NULL, 0}
 };
