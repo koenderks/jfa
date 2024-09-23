5c45207f70302128ba078dc998c63d63ed49687c
federicapicogna <128388440+federicapicogna@users.noreply.github.com>
2024-09-23 13:03:14 +0200

    changed description

--- R/fairness_measure_workflow.R
+++ R/fairness_measure_workflow.R
@@ -13,9 +13,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-#' Algorithm Auditing: Fairness Metrics and Parity
+#' Algorithm Auditing: Fairness Metrics Decision-Making Workflow
 #'
-#' @description This function aims to assess fairness in algorithmic
+#' @description DESCRIZIONE DI COSA FA LA FUNZIONE This function aims to assess fairness in algorithmic
 #' decision-making systems by computing and testing the equality of one of
 #' several model-agnostic fairness metrics between protected classes. The
 #' metrics are computed based on a set of true labels and the predictions of an
@@ -45,7 +45,7 @@
 #'   prior = FALSE
 #' )
 #'
-#' @param data         a data frame containing the input data.
+#' @param data        DESCRIZIONE ARGOMENTI FUNZIONE a data frame containing the input data.
 #' @param protected    a character specifying the column name in \code{data}
 #'   containing the protected classes (i.e., the sensitive attribute).
 #' @param target       a character specifying the column name in \code{data}
@@ -74,49 +74,13 @@
 #'   Bayesian estimation using a default prior with concentration parameter 1 is
 #'   performed.
 #'
-#' @details The following model-agnostic fairness metrics are computed based on
-#'   the confusion matrix for each protected class, using the true positives
-#'   (TP), false positives (FP), true negative (TN) and false negatives (FN).
-#'   See Pessach & Shmueli (2022) for a more detailed explanation of the
-#'   individual metrics. The equality of metrics across groups is done according
-#'   to the methodology described in Fisher (1970) and Jamil et al. (2017).
-#'
-#'   \itemize{
-#'     \item{Predictive rate parity (\code{prp}): calculated as TP / (TP +
-#'       FP), its ratio quantifies whether the predictive rate is equal across
-#'       protected classes.}
-#'     \item{Proportional parity (\code{pp}): calculated as (TP + FP) / (TP +
-#'       FP + TN + FN), its ratio quantifies whether the positive prediction
-#'       rate is equal across protected classes.}
-#'     \item{Accuracy parity (\code{ap}): calculated as (TP + TN) / (TP + FP +
-#'       TN + FN), quantifies whether the accuracy is the same across groups.}
-#'     \item{False negative rate parity (\code{fnrp}): calculated as FN / (FP
-#'       + FN), quantifies whether the false negative rate is the same across
-#'       groups.}
-#'     \item{False positive rate parity (\code{fprp}): calculated as FP / (TN
-#'       + FP), quantifies whether the false positive rate is the same across
-#'       groups.}
-#'     \item{True positive rate parity (\code{tprp}): calculated as TP / (TP +
-#'       FN), quantifies whether the true positive rate is the same across
-#'       groups.}
-#'     \item{Negative predicted value parity (\code{npvp}): calculated as TN /
-#'       (TN + FN), quantifies whether the negative predicted value is equal
-#'       across groups.}
-#'     \item{Specificity parity (\code{sp}): calculated as TN / (TN + FP),
-#'       quantifies whether the true positive rate is the same across groups.}
-#'     \item{Demographic parity (\code{dp}): calculated as TP + FP, quantifies
-#'       whether the positive predictions are equal across groups.}
-#'   }
-#'
-#'   Note that, in an audit context, not all fairness measures are equally
-#'   appropriate in all situations. The fairness tree below aids in choosing
-#'   which fairness measure is appropriate for the situation at hand (Büyük,
-#'   2023).
+#' @details The fairness decision-making workflow below aids in choosing
+#'   which fairness measure is appropriate for the situation at hand. AGGIUNGERE RIFERIMENTI
 #'
 #'   \if{html}{\figure{fairness-tree.png}{options: width="100\%" alt="fairness-tree"}}
 #'   \if{latex}{\figure{fairness-tree.pdf}{options: width=5in}}
 #'
-#' @return An object of class \code{jfaFairness} containing:
+#' @return An object of class \code{jfaFairness} containing: 
 #'
 #' \item{data}{the specified data.}
 #' \item{conf.level}{a numeric value between 0 and 1 giving the confidence
@@ -141,7 +105,8 @@
 #'   odds ratio of the fairness metric and its associated confidence/credible
 #'   interval, along with inferential measures such as uncorrected p-values or
 #'   Bayes factors.}
-#' \item{measure}{The abbreviation of the selected fairness metric.}
+#' \item{measure}{The abbreviation of the selected fairness measure}
+#' \item{name}{The full name of the selected fairness measure}
 #' \item{prior}{a logical indicating whether a prior distribution was used.}
 #' \item{data.name}{The name of the input data object.}
 #'
@@ -178,7 +143,7 @@
 #'   \emph{Proceedings of the 26th International Conference on World Wide Web}.
 #'   \doi{10.1145/3038912.3052660}
 #'
-#' @keywords algorithm audit bias fairness model performance
+#' @keywords algorithm audit bias fairness workflow 
 #'
 #' @examples
 #' # Frequentist test of specificy parity
@@ -193,7 +158,7 @@
 #' )
 #' @export
 
-DecisionMakingWorkflow <- function(q1 = NULL, q2 = NULL, q3 = NULL, q4 = NULL) {
+fairness_measure_workflow <- function(q1 = NULL, q2 = NULL, q3 = NULL, q4 = NULL) {
   if (is.null(q1)) {
     q1 <- utils::menu(choices = c("Yes", "No"), title = "Do you have the information on the true values of the classification?")
   } else {
@@ -228,36 +193,36 @@ DecisionMakingWorkflow <- function(q1 = NULL, q2 = NULL, q3 = NULL, q4 = NULL) {
 
   if (q1 == 2) {
     name <- "Disparate Impact"
-    metric <- "di"
+    measure <- "di"
   } else if (q1 == 1) {
     if (q2 == 3) {
       name <- "Equalized Odds"
-      metric <- "eo"
+      measure <- "eo"
     } else {
       if (q2 == 1) {
         if (q3 == 1) {
           if (q4 == 1) {
             name <- "Predictive Rate Parity"
-            metric <- "prp"
+            measure <- "prp"
           } else if (q4 == 2) {
             name <- "Equal Opportunity"
-            metric <- "eo"
+            measure <- "eo"
           } else if (q4 == 3) {
             name <- "No measure with these characteristics is available in the Decision-Making Workflow if the Negative Class is Everything not positive"
-            metric <- "error"
+            measure <- "error"
           } else {
             stop("Invalid imput:  The value of the fourth argument must be 1 (to indicate 'False Positive'), 2 (to indicate 'False Negative'), or 3 (to indicate 'No preference').")
           }
         } else if (q3 == 2) {
           if (q4 == 1) {
             name <- "Specificity Parity"
-            metric <- "sp"
+            measure <- "sp"
           } else if (q4 == 2) {
             name <- "Negative Predictive Rate Parity"
-            metric <- "nprp"
+            measure <- "nprp"
           } else if (q4 == 3) {
             name <- "Accuracy Parity"
-            metric <- "ap"
+            measure <- "ap"
           } else {
             stop("Invalid imput:  The value of the fourth argument must be 1 (to indicate 'False Positive'), 2 (to indicate 'False Negative'), or 3 (to indicate 'No preference').")
           }
@@ -267,13 +232,13 @@ DecisionMakingWorkflow <- function(q1 = NULL, q2 = NULL, q3 = NULL, q4 = NULL) {
       } else if (q2 == 2) {
         if (q4 == 1) {
           name <- "False Positive Rate Parity"
-          metric <- "fprp"
+          measure <- "fprp"
         } else if (q4 == 2) {
           name <- "False Negative Rate Parity"
-          metric <- "fnrp"
+          measure <- "fnrp"
         } else if (q4 == 3) {
           name <- "No measure with these characteristics is available in the Decision-Making Workflow if the interest is in the incorrect classification"
-          metric <- "error"
+          measure <- "error"
         } else {
           stop("Invalid imput:  The value of the fourth argument must be 1 (to indicate 'False Positive'), 2 (to indicate 'False Negative'), or 3 (to indicate 'No preference').")
         }
@@ -286,7 +251,7 @@ DecisionMakingWorkflow <- function(q1 = NULL, q2 = NULL, q3 = NULL, q4 = NULL) {
   }
 
   output <- list()
-  output[["metric"]] <- metric
+  output[["measure"]] <- measure
   output[["name"]] <- name
   # output[["qq"]]<- qq
   class(output) <- c(class(output), "jfaFairnessWorkflow")
--- R/model_fairness.R
+++ R/model_fairness.R
@@ -206,7 +206,11 @@ model_fairness <- function(data,
                            alternative = c("two.sided", "less", "greater"),
                            conf.level = 0.95,
                            prior = FALSE) {
-  metric <- match.arg(metric)
+  if (inherits(metric, "jfaFairnessWorkflow")) {
+    metric <- metric[["measure"]]
+  } else {
+    metric <- match.arg(metric)
+  }
   alternative <- match.arg(alternative)
   dname <- deparse(substitute(data))
   data <- as.data.frame(data, row.names = seq_len(nrow(data)))
