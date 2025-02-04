166ecc226061c7a47873b5698e784fce81b9a71f
federicapicogna <128388440+federicapicogna@users.noreply.github.com>
2025-02-04 14:02:19 +0100

    update legend

--- R/jfa-methods.R
+++ R/jfa-methods.R
@@ -1524,7 +1524,7 @@ print.jfaFairnessSelection <- function(x, ...) {
 #' @method plot jfaFairnessSelection
 #' @export
 plot.jfaFairnessSelection <- function(x, ...) {
-  xmin <- xmax <- ymin <- ymax <- label <- fill <- text_color <- NULL
+  xmin <- xmax <- ymin <- ymax <- label <- text_color <- NULL
   measure <- x$name
   if (measure == "Predictive Rate Parity") {
     rects <- data.frame(
